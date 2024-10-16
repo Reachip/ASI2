de76291680f5c72fd11db6896c6d34a94fb144fd
Rached Mejri <r.mejri74100@gmail.com>
2024-10-16 11:26:54 +0200

    création des packages

---
+++ atelier-1/pictureMS/src/test/java/atelier1/groupe3/picturems/PictureMsApplicationTests.java
@@ -0,0 +1,13 @@
+package atelier1.groupe3.picturems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ atelier-1/textMS/src/main/java/atelier1/groupe3/textms/TextMsApplication.java
@@ -0,0 +1,13 @@
+package atelier1.groupe3.textms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextMsApplication.class, args);
+    }
+
+}
