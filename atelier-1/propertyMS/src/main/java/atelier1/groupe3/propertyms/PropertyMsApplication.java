bb9276ed390707eea0e963b8a0fd38d7d2817d13
Rached Mejri <r.mejri74100@gmail.com>
2024-10-16 11:30:33 +0200

    test
    Ajout d'un package

---
+++ atelier-1/propertyMS/src/main/java/atelier1/groupe3/propertyms/PropertyMsApplication.java
@@ -0,0 +1,13 @@
+package atelier1.groupe3.propertyms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyMsApplication.class, args);
+    }
+
+}
