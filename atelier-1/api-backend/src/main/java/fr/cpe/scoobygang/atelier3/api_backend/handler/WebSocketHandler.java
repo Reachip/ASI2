65cc5b9b4f7e2e9eaea164bb3b0398db71138951
Rached Mejri <r.mejri74100@gmail.com>
2024-10-18 13:38:55 +0200

    Mise en place du serveur WS

---
+++ atelier-1/api-backend/src/main/java/fr/cpe/scoobygang/atelier3/api_backend/handler/WebSocketHandler.java
@@ -0,0 +1,43 @@
+package fr.cpe.scoobygang.atelier3.api_backend.handler;
+
+
+import fr.cpe.scoobygang.common.websocket.WSMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Service
+public final class WebSocketHandler extends TextWebSocketHandler {
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketHandler.class);
+    private final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(final WebSocketSession session) {
+        logger.info("Connexion établie");
+
+        logger.info("Nombre de sessions : " + sessions.size());
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        logger.info("Connexion fermée");
+        sessions.remove(session);
+        logger.info("Nombre de sessions : " + sessions.size());
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) {
+        logger.info("Récéption d'un message : " + message.getPayload());
+    }
+
+    public void broadcastMessage(final WSMessage message) {
+        // TODO: Appeler cette méthode quand on a la réponse
+        //  de l'orchestrateur qui nous signal via requête POST que tout est terminé ...
+    }
+}
---
+++ atelier-1/api-common/src/main/java/fr/cpe/scoobygang/common/websocket/WSMessage.java
@@ -0,0 +1,11 @@
+package fr.cpe.scoobygang.common.websocket;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class WSMessage implements Serializable {
+}
