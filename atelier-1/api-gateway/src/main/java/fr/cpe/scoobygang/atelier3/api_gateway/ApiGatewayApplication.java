3dfd370090fb763bc535c035fdb14b97fdd060b9
Rached Mejri <rached.mejri@orange.com>
2024-10-28 18:27:57 +0100

    Servir le frontend et le monolithe

--- atelier-1/api-gateway/src/main/java/fr/cpe/scoobygang/atelier3/api_gateway/ApiGatewayApplication.java
+++ atelier-1/api-gateway/src/main/java/fr/cpe/scoobygang/atelier3/api_gateway/ApiGatewayApplication.java
@@ -23,11 +23,11 @@ public class ApiGatewayApplication {
 
 	@Bean
 	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-		// .route("card-route", r -> r.path("/card/**").uri("lb://api-card-microservice"))
-
 		return builder
 				.routes()
+				.route("backend-route", r -> r.path("/api/**").uri("lb://api-backend"))
 				.route("orchestrator-route", r -> r.path("/orchestrator/**").uri("lb://api-orchestrator-microservice"))
+				.route("react-app-route", r -> r.path("/**").uri("http://localhost:80/"))
 				.build();
 	}
 
