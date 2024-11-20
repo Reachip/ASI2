85aef6405fb833c282b704f98d5713e1c8c7e268
Eliott_C <eliott.caumon@cpe.fr>
2024-11-20 14:28:52 +0100

    attack event

--- atelier-2/backend-node/events/setRewardAmountEvent.mjs
+++ atelier-2/backend-node/events/setRewardAmountEvent.mjs
@@ -2,7 +2,26 @@ import { GAME_HASH } from "../utils/constants.mjs";
 
 // data : {gameId, rewardAmount}
 const setRewardAmountEvent = async (redis, io, socket, data) => {
+    if (data === undefined || data === null)
+    {
+        return console.log("Error: gameId and rewardAmount are required.");
+    }
 
+    const gameId = data.gameId;
+    const rewardAmount = data.rewardAmount;
+
+    const game = await redis.hget(GAME_HASH, gameId);
+    if (!game)
+    {
+        return console.log("Error: Game not found.");
+    }
+
+    const gameData = JSON.parse(game);
+    gameData.rewardAmount = rewardAmount;
+    
+    await redis.hset(GAME_HASH, gameId, JSON.stringify(gameData));
+
+    io.to(gameId).emit("rewardAmountUpdated", { rewardAmount });
 }
 
 export default setRewardAmountEvent;
