aab866ad834f8057afc2e9b0dcdc9f97c1a06d3d
JorisGarcia19 <garcia.joris@outlook.fr>
2024-11-27 23:54:10 +0100

    Suppresion de la game dans redis si un utilisateur se deconecte

--- atelier-2/backend-node/utils/redisUtils.mjs
+++ atelier-2/backend-node/utils/redisUtils.mjs
@@ -1,4 +1,10 @@
-import {CONNECTED_USERS_HASH, SELECTED_USER_HASH, USER_ROOMS_HASH, WAITLIST_FIGHT_HASH} from "./constants.mjs";
+import {
+    CONNECTED_USERS_HASH,
+    GAME_HASH,
+    SELECTED_USER_HASH,
+    USER_ROOMS_HASH,
+    WAITLIST_FIGHT_HASH
+} from "./constants.mjs";
 
 export const addInRedis = async (redis, hash, key, value) => {
     try {
@@ -124,8 +130,7 @@ export async function initServer(io, redis) {
     await redis.del(CONNECTED_USERS_HASH);
     await redis.del(SELECTED_USER_HASH);
     await redis.del(USER_ROOMS_HASH);
-
-    // Log details from Redis
+    await redis.del(GAME_HASH);
     await logDetailsRedis(io, redis);
 
     console.log("init Server completed");
