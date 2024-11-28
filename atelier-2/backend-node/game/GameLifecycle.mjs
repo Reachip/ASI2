d036d37a6175e84d8a9efce574756b2d051c00ac
Rached Mejri <r.mejri74100@gmail.com>
2024-11-28 22:16:12 +0100

    Fix récupération de carte

--- atelier-2/backend-node/game/GameLifecycle.mjs
+++ atelier-2/backend-node/game/GameLifecycle.mjs
@@ -13,7 +13,11 @@ class GameLifecycle {
     async _getOpponentCard(cardOpponentId) {
         console.log('[GameLifecycle] Getting opponent card:', cardOpponentId);
         const game = await this.game.get()
-        const card = game.user1.userId == game.userTurn ? game.user2.cards.find(card => card.id === cardOpponentId) : game.user1.cards.find(card => card.id === cardOpponentId);
+
+        const card = [game.user1.cards,  game.user2.cards]
+            .flat()
+            .find(card => card.id === cardOpponentId);
+
         console.log('[GameLifecycle] Found opponent card:', card);
         return card;
     }
@@ -28,9 +32,14 @@ class GameLifecycle {
 
     async _getCurrentPlayerCard(cardPlayerId) {
         console.log('[GameLifecycle] Getting current player card:', cardPlayerId);
+
         const game = await this.game.get()
-        const card = game.user1.userId == game.userTurn ? game.user1.cards.find(card => card.id === cardPlayerId) : game.user2.cards.find(card => card.id === cardPlayerId);
+        const card = [game.user1.cards,  game.user2.cards]
+            .flat()
+            .find(card => card.id === cardPlayerId);
+
         console.log('[GameLifecycle] Found current player card:', card);
+
         return card;
     }
 
@@ -44,6 +53,7 @@ class GameLifecycle {
 
     async attack() {
         const attackLabel = {}
+
         console.log('[GameLifecycle] Initiating attack');
 
         const cardAttack = await this._getCurrentPlayerCard(this.currentCardPlayerId)
